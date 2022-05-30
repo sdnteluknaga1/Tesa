13bc3201bc1fc91bc63682dca1abf38b7620a9c9
sdnteluknaga1 <@sdn.teluknaga1@gmail.com>
2022-05-30 15:47:57 +0700

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container : {
+      center : true,
+      padding : '16px',
+    },
+    extend: {
+      colors : {
+        wtit : '#035397',
+        wtitt : '#2dd4bf',
+        wpar : '#475569',
+        wpara : '#6b7280',
+        bg1 : '#F9EBC8',
+        bg2 : '#FEFBE7',
+        bg3 : '#DAE5D0',
+        bg4 : 'A0BCC2',
+      },
+      screen : {
+        '2xl' : '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
